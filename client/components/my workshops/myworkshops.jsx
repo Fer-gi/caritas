69f9c3ec3226baa1a123a714427b1d354839baa3
Emily <emily.artfree@gmail.com>
2023-11-14 16:18:32 +0100

    (fix myworkshop)

--- client/components/my workshops/myworkshops.jsx
+++ client/components/my workshops/myworkshops.jsx
@@ -28,17 +28,25 @@ export const MyWorkshopsComponent = () =>{
     return (
       <>
       <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+      {myWorkshops.map((myworkshop) => (
+      <Card.Body key={myworkshop.id}>
+        <Card.Title>{myworkshop.name}</Card.Title>
+        <Card.Subtitle className="">plazas: {myworkshop.spots}</Card.Subtitle>
+        <Card.Text className= "location">
+         {myworkshop.location} {myworkshop.mandatory}
         </Card.Text>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
+        <Card.Text className = "days">
+          <img className='img_icon' src="" alt="" />
+         {myworkshop.days} {myworkshop.time}
+        </Card.Text>
       </Card.Body>
+      ))}
     </Card>
+
+    <section>
+      <button>chat</button>
+    </section>
+  
       </>
     )
 }
