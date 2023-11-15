0d925814ce503382b809f69a9d529e3ac34f1429
Emily <emily.artfree@gmail.com>
2023-11-15 15:30:58 +0100

    fix(compilation/navbar)

--- client/components/navbar/navbar.jsx
+++ client/components/navbar/navbar.jsx
@@ -7,7 +7,7 @@ import LogoCaritas from "../../assets/img/LogoCaritas.svg";
 function Caritasnavbar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-      <Container className='nav'>
+      <Container fluid className='nav'>
         <Navbar.Brand href="#home"><img src={LogoCaritas} alt="logo" className='LogoNav'/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
