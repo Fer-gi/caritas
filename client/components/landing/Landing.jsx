3f698e1306ce1fdee75f72d92bb42e9ca27af343
Luis <bamcuent312@gmail.com>
2023-11-15 16:38:02 +0100

    feat(compilation) add style landing

--- client/components/landing/Landing.jsx
+++ client/components/landing/Landing.jsx
@@ -1,11 +1,22 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import LogoCampus from '../../assets/img/LogoCampus.svg'
+import LogoCampus from '../../assets/img/LogoCampus.svg';
+import './Landing.css'
 const Landing = () => {
-  const burgundyColor = '#800020';
   const navigate = useNavigate();
+
   useEffect(() => {
-    const timer = setTimeout(() => { navigate('/welcome'); },
-      3000); return () => clearTimeout(timer);
-  }, [navigate]); return (<div style={{ backgroundColor: burgundyColor, color: 'white', height: '80vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}> <img src={LogoCampus} ></img> </div>);
-}; export default Landing;
+    const timer = setTimeout(() => {
+      navigate('/welcome');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <div className="landing-container bg-cd222d text-white d-flex justify-content-center align-items-center" style={{ height: '80vh' }}>
+      <img src={LogoCampus} className="img-fluid" alt="Logo" />
+    </div>
+  );
+};
+
+export default Landing;
