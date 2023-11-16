0ddf956c33d940a159689bbc0047a0b215d5cc1b
Diego Bordallo <bordallo.d@gmail.com>
2023-11-16 10:20:33 +0100

    feat(studenthome)create studenthome styles

--- client/components/home/StudentHome.jsx
+++ client/components/home/StudentHome.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'; 
-import avatar from '../../assets/img/avatar.jpeg';
+import light from '../../assets/img/light.gif';
 import { useParams, Link} from 'react-router-dom';
+import "./StudentHome.css"
 
 function StudentHome() {
     const {id}  = useParams();
     return (
-      <div className='container'>
-        <img src={avatar} alt="Avatar" className='avatar' />
-        <div className="d-grid gap-2">
+      <div className='container menu'>
+        <img src={light} alt="Avatar" className='avatar' />
+        <div className="d-grid gap-2 btnsVL" >
             <Link to={"orientacion-vocacional"}>
-          <Button variant="danger" size="lg" className='o-vocacional'>
+          <Button variant="danger" size="lg" className='o-vocacional btns'>
             Orientación Vocacional      
           </Button>
           </Link>
           <Link to={"orientacion-laboral"}>
-          <Button variant="danger" size="lg" className='o-laboral'>
+          <Button variant="danger" size="lg" className='o-laboral btns'>
             Orientación Laboral
           </Button>
           </Link>
