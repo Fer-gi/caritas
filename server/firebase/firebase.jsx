27d26296c5d6ed3ab8283adc10934af5e5784925
Diego Bordallo <bordallo.d@gmail.com>
2023-11-17 12:11:10 +0100

    fix(firebase)move firebase to server folder

---
+++ server/firebase/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAuth } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_APP_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_APP_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_APPID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
+
